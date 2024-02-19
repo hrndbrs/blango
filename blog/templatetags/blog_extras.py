d2a849724771f4d398f8e097d34ce089db9790b7
Hernando Hernando <hrndbrs@gmail.com>
2024-02-19 20:49:49 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,3 +1,4 @@
+import logging
 from django.utils.html import format_html
 # from django.utils.html import escape
 # from django.utils.safestring import mark_safe
@@ -8,6 +9,8 @@ from blog.models import Post
 user_model = get_user_model()
 register = template.Library()
 
+logger = logging.getLogger(__name__)
+
 @register.filter
 def author_details(author, current_user=None):
   if not isinstance(author, user_model):
@@ -17,38 +20,39 @@ def author_details(author, current_user=None):
     return format_html("<strong>me</strong>")
   
   if author.first_name and author.last_name:
-      name = f"{author.first_name} {author.last_name}"
+    name = f"{author.first_name} {author.last_name}"
   else:
-      name = f"{author.username}"
+    name = f"{author.username}"
 
   if author.email:
-      prefix = format_html('<a href="mailto:{}">', author.email)
-      suffix = format_html("</a>")
+    prefix = format_html('<a href="mailto:{}">', author.email)
+    suffix = format_html("</a>")
   else:
-      prefix = ""
-      suffix = ""
+    prefix = ""
+    suffix = ""
 
   return format_html('{}{}{}', prefix, name, suffix)
 
 @register.simple_tag
 def row(extra_classes=""):
-    return format_html('<div class="row {}">', extra_classes)
+  return format_html('<div class="row {}">', extra_classes)
 
 
 @register.simple_tag
 def endrow():
-    return format_html("</div>")
+  return format_html("</div>")
 
 @register.simple_tag
 def col(extra_classes=""):
-    return format_html('<div class="col {}">', extra_classes)
+  return format_html('<div class="col {}">', extra_classes)
 
 
 @register.simple_tag
 def endcol():
-    return format_html("</div>")
+  return format_html("</div>")
 
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
-    posts = Post.objects.exclude(pk=post.pk)[:5]
-    return {"title": "Recent Posts", "posts": posts}
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
+  return {"title": "Recent Posts", "posts": posts}
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,16 @@
 import logging
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+# from django.views.decorators.vary import vary_on_cookie
+# from django.views.decorators.cache import cache_page
 from blog.models import Post
 from blog.forms import CommentForm
 
 # Create your views here.
 logger = logging.getLogger(__name__)
 
+# @cache_page(300) # Time in seconds
+# @vary_on_cookie
 def index(req):
   posts = Post.objects.filter(published_at__lte=timezone.now())
 
