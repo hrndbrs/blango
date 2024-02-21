6773a18ac9adcf0008c6bf9344cbf1bf7584284d
Hernando Hernando <hrndbrs@gmail.com>
2024-02-21 14:16:35 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -115,6 +115,7 @@ class Dev(Configuration):
         "allauth.account",
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
+        "rest_framework",
     ]
 
     MIDDLEWARE = [
--- blog/api_views.py
+++ blog/api_views.py
@@ -7,30 +7,35 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from blog.models import Post
+from blog.api.serializers import PostSerializer
 
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
+# def post_to_dict(post):
+#     return {
+#         "pk": post.pk,
+#         "author_id": post.author_id,
+#         "created_at": post.created_at,
+#         "modified_at": post.modified_at,
+#         "published_at": post.published_at,
+#         "title": post.title,
+#         "slug": post.slug,
+#         "summary": post.summary,
+#         "content": post.content,
+#     }
 
 @csrf_exempt
 def post_list(req):
   if req.method == "GET":
-    posts = [post_to_dict(post) for post in Post.objects.all()]
+    # posts = [post_to_dict(post) for post in Post.objects.all()]
+    posts = Post.objects.all()
     
-    return JsonResponse({"data": posts})
+    return JsonResponse({"data": PostSerializer(posts, many=True).data})
 
   elif req.method == "POST":
     post_data = json.loads(req.body)
-    post = Post.objects.create(**post_data)
+    # post = Post.objects.create(**post_data)
+    s = PostSerializer(data=post_data)
+    s.is_valid(raise_exception=True)
+    post = s.save()
 
     return HttpResponse(
       status=HTTPStatus.CREATED,
@@ -44,13 +49,18 @@ def post_detail(req, pk):
   post = get_object_or_404(Post, pk=pk)
 
   if req.method == "GET":
-    return JsonResponse(post_to_dict(post))
+    # return JsonResponse(post_to_dict(post))
+    return JsonResponse(PostSerializer(post).data)
 
   elif req.method == "PUT":
     post_data = json.loads(req.body)
-    for field, value in post_data.items():
-      setattr(post, field, value)
-    post.save()
+    # for field, value in post_data.items():
+    #   setattr(post, field, value)
+    # post.save()
+
+    s = PostSerializer(post, data=post_data)
+    s.is_valid(raise_exception=True)
+    s.save()
 
     return HttpResponse(status=HTTPStatus.NO_CONTENT)
 
