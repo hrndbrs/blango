373509f5915be14b5a1d7716e79d0bd41350b6e5
Hernando Hernando <hrndbrs@gmail.com>
2024-02-27 00:50:39 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostDetail, PostList, UserDetail
+from rest_framework.routers import DefaultRouter
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -18,8 +23,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = format_suffix_patterns([
-  path("posts/", PostList.as_view(), name="api_post_list"),
-  path("posts/<int:pk>/", PostDetail.as_view(), name="api_post_detail"),
+  # path("posts/", PostList.as_view(), name="api_post_list"),
+  # path("posts/<int:pk>/", PostDetail.as_view(), name="api_post_detail"),
   path("auth/", include("rest_framework.urls")),
   path("token-auth/", views.obtain_auth_token),
   path("users/<str:email>/", UserDetail.as_view(), name="api_user_detail")
@@ -38,4 +43,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,8 +1,18 @@
-from rest_framework import generics
-from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
+from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from blog.api.serializers import (
+    PostSerializer,
+    UserSerializer,
+    PostDetailSerializer,
+    TagSerializer,
+)
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.models import Post
+from blog.models import Post, Tag
 from blango_auth.models import User
+
+"""
+Leaving it here for learning purposes
 
 class PostList(generics.ListCreateAPIView):
   queryset = Post.objects.all()
@@ -12,8 +22,32 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
   serializer_class = PostDetailSerializer
+"""
 
 class UserDetail(generics.RetrieveAPIView):
   queryset = User.objects.all()
   serializer_class = UserSerializer
   lookup_field = "email"
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    @action(methods=["get"], detail=True, name="Posts with the tag")
+    def posts(self, req, pk=None):
+      tag = self.get_object()
+      post_serializer = PostSerializer(
+        tag.posts, many=True, context = {"request": req}
+      )
+
+      return Response(post_serializer.data)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+  permission_classes = [AuthorModifyOrReadOnly|IsAdminUserForObject]
+  queryset = Post.objects.all()
+
+  def get_serializer_class(self):
+    if self.action in ("list", "create"):
+      return PostSerializer
+    return PostDetailSerializer
